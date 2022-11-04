3baa9249742e180510f56f22694b10bcdacb14af
DANGEROUS <110321607+DANI2999@users.noreply.github.com>
2022-11-04 21:39:15 +0400

    style2

--- js/script.js
+++ js/script.js
@@ -1,15 +1,24 @@
 let renderer,
-scene,
-camera,
-sphereBg,
-nucleus,
-stars,
-controls,
-container = document.getElementById("canvas_container"),
-timeout_Debounce,
-noise = new SimplexNoise(),
-cameraSpeed = 0,
-blobScale = 3;
+    camera,
+    planet,
+    moon,
+    sphereBg,
+    terrainGeometry,
+    container = document.getElementById("canvas_container"),
+    timeout_Debounce,
+    frame = 0,
+    cameraDx = 0.05,
+    count = 0,
+    t = 0;
+
+/*   Lines values  */
+let lineTotal = 1000;
+let linesGeometry = new THREE.BufferGeometry();
+linesGeometry.setAttribute("position", new THREE.BufferAttribute(new Float32Array(6 * lineTotal), 3));
+linesGeometry.setAttribute("velocity", new THREE.BufferAttribute(new Float32Array(2 * lineTotal), 1));
+let l_positionAttr = linesGeometry.getAttribute("position");
+let l_vertex_Array = linesGeometry.getAttribute("position").array;
+let l_velocity_Array = linesGeometry.getAttribute("velocity").array;
 
 
 init();
@@ -18,17 +27,15 @@ animate();
 
 function init() {
     scene = new THREE.Scene();
+    scene.background = new THREE.Color("#000000");
+    scene.fog = new THREE.Fog("#3c1e02", 0.5, 50);
 
     camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 0.01, 1000)
-    camera.position.set(0,0,230);
+    camera.position.set(0, 1, 32)
 
-    const directionalLight = new THREE.DirectionalLight("#fff", 2);
-    directionalLight.position.set(0, 50, -20);
-    scene.add(directionalLight);
-
-    let ambientLight = new THREE.AmbientLight("#ffffff", 1);
-    ambientLight.position.set(0, 20, 20);
-    scene.add(ambientLight);
+    pointLight1 = new THREE.PointLight("#ffffff", 1, 0);
+    pointLight1.position.set(0, 30, 30);
+    scene.add(pointLight1);
 
     renderer = new THREE.WebGLRenderer({
         antialias: true,
@@ -38,149 +45,171 @@ function init() {
     renderer.setPixelRatio(window.devicePixelRatio);
     container.appendChild(renderer.domElement);
 
-    //OrbitControl
-    controls = new THREE.OrbitControls(camera, renderer.domElement);
-    controls.autoRotate = true;
-    controls.autoRotateSpeed = 4;
-    controls.maxDistance = 350;
-    controls.minDistance = 150;
-    controls.enablePan = false;
-
     const loader = new THREE.TextureLoader();
-    const textureSphereBg = loader.load('https://i.ibb.co/4gHcRZD/bg3-je3ddz.jpg');
-    const texturenucleus = loader.load('https://i.ibb.co/hcN2qXk/star-nc8wkw.jpg');
-    const textureStar = loader.load("https://i.ibb.co/ZKsdYSz/p1-g3zb2a.png");
-    const texture1 = loader.load("https://i.ibb.co/F8by6wW/p2-b3gnym.png");  
-    const texture2 = loader.load("https://i.ibb.co/yYS2yx5/p3-ttfn70.png");
-    const texture4 = loader.load("https://i.ibb.co/yWfKkHh/p4-avirap.png");
+
+    // Planet
+    const texturePlanet = loader.load('https://i.ibb.co/h94JBXy/saturn3-ljge5g.jpg');
+    texturePlanet.anisotropy = 16;
+    const planetGeometry = new THREE.SphereBufferGeometry(10, 50, 50);
+    const planetMaterial = new THREE.MeshLambertMaterial({
+        map: texturePlanet,
+        fog: false
+    });
+    planet = new THREE.Mesh(planetGeometry, planetMaterial);
+    planet.position.set(0, 8, -30);
+    scene.add(planet);
 
 
-    /*  Nucleus  */   
-    texturenucleus.anisotropy = 16;
-    let icosahedronGeometry = new THREE.IcosahedronGeometry(30, 10);
-    let lambertMaterial = new THREE.MeshPhongMaterial({ map: texturenucleus });
-    nucleus = new THREE.Mesh(icosahedronGeometry, lambertMaterial);
-    scene.add(nucleus);
+    //Moon
+    const textureMoon = loader.load('https://i.ibb.co/64zn361/moon-ndengb.jpg');
+    textureMoon.anisotropy = 16;
+    let moonGeometry = new THREE.SphereBufferGeometry(2, 32, 32);
+    let moonMaterial = new THREE.MeshPhongMaterial({
+        map: textureMoon,
+        fog: false
+    });
+    moon = new THREE.Mesh(moonGeometry, moonMaterial);
+    moon.position.set(0, 8, 0);
+    scene.add(moon);
 
 
-    /*    Sphere  Background   */
+    // Sphere Background 
+    const textureSphereBg = loader.load('https://i.ibb.co/JCsHJpp/stars2-qx9prz.jpg');
     textureSphereBg.anisotropy = 16;
-    let geometrySphereBg = new THREE.SphereBufferGeometry(150, 40, 40);
-    let materialSphereBg = new THREE.MeshBasicMaterial({
+    const geometrySphereBg = new THREE.SphereBufferGeometry(150, 32, 32);
+    const materialSphereBg = new THREE.MeshBasicMaterial({
         side: THREE.BackSide,
         map: textureSphereBg,
+        fog: false
     });
     sphereBg = new THREE.Mesh(geometrySphereBg, materialSphereBg);
+    sphereBg.position.set(0, 50, 0);
     scene.add(sphereBg);
 
 
-    /*    Moving Stars   */
-    let starsGeometry = new THREE.Geometry();
+    // Terrain
+    const textureTerrain = loader.load();
+    textureTerrain.rotation = THREE.MathUtils.degToRad(5);
+    terrainGeometry = new THREE.PlaneBufferGeometry(70, 70, 20, 20);
+    const terrainMaterial = new THREE.MeshBasicMaterial({
+        map: textureTerrain,
+        fog: true
+    });
+    terrain = new THREE.Mesh(terrainGeometry, terrainMaterial);
+    terrain.rotation.x = -0.47 * Math.PI;
+    terrain.rotation.z = THREE.Math.degToRad(90);
+    scene.add(terrain);
 
-    for (let i = 0; i < 50; i++) {
-        let particleStar = randomPointSphere(150); 
+    let t_vertex_Array = terrainGeometry.getAttribute("position").array;
+    terrainGeometry.getAttribute("position").setUsage(THREE.DynamicDrawUsage);
 
-        particleStar.velocity = THREE.MathUtils.randInt(50, 200);
+    terrainGeometry.setAttribute("myZ", new THREE.BufferAttribute(new Float32Array(t_vertex_Array.length / 3), 1));
+    let t_myZ_Array = terrainGeometry.getAttribute("myZ").array;
 
-        particleStar.startX = particleStar.x;
-        particleStar.startY = particleStar.y;
-        particleStar.startZ = particleStar.z;
-
-        starsGeometry.vertices.push(particleStar);
+    for (let i = 0; i < t_vertex_Array.length; i++) {
+        t_myZ_Array[i] = THREE.MathUtils.randInt(0, 5);
     }
-    let starsMaterial = new THREE.PointsMaterial({
-        size: 5,
+
+    //Terain Lines
+    const terrain_line = new THREE.LineSegments(
+        terrainGeometry,
+        new THREE.LineBasicMaterial({
+            color: "#fff",
+            fog: false
+        })
+    );
+    terrain_line.rotation.x = -0.47 * Math.PI;
+    terrain_line.rotation.z = THREE.Math.degToRad(90);
+    scene.add(terrain_line);
+
+
+
+    //  Stars
+    for (let i = 0; i < lineTotal; i++) {
+        let x = THREE.MathUtils.randInt(-100, 100);
+        let y = THREE.MathUtils.randInt(10, 40);
+        if (x < 7 && x > -7 && y < 20) x += 14;
+        let z = THREE.MathUtils.randInt(0, -300);
+
+        l_vertex_Array[6 * i + 0] = l_vertex_Array[6 * i + 3] = x;
+        l_vertex_Array[6 * i + 1] = l_vertex_Array[6 * i + 4] = y;
+        l_vertex_Array[6 * i + 2] = l_vertex_Array[6 * i + 5] = z;
+
+        l_velocity_Array[2 * i] = l_velocity_Array[2 * i + 1] = 0;
+    }
+    let starsMaterial = new THREE.LineBasicMaterial({
         color: "#ffffff",
         transparent: true,
-        opacity: 0.8,
-        map: textureStar,
-        blending: THREE.AdditiveBlending,
+        opacity: 0.5,
+        fog: false
     });
-    starsMaterial.depthWrite = false;  
-    stars = new THREE.Points(starsGeometry, starsMaterial);
-    scene.add(stars);
-
-
-    /*    Fixed Stars   */
-    function createStars(texture, size, total) {
-        let pointGeometry = new THREE.Geometry();
-        let pointMaterial = new THREE.PointsMaterial({
-            size: size,
-            map: texture,
-            blending: THREE.AdditiveBlending,                      
-        });
-
-        for (let i = 0; i < total; i++) {
-            let radius = THREE.MathUtils.randInt(149, 70); 
-            let particles = randomPointSphere(radius);
-            pointGeometry.vertices.push(particles);
-        }
-        return new THREE.Points(pointGeometry, pointMaterial);
-    }
-    scene.add(createStars(texture1, 15, 20));   
-    scene.add(createStars(texture2, 5, 5));
-    scene.add(createStars(texture4, 7, 5));
-
-
-    function randomPointSphere (radius) {
-        let theta = 2 * Math.PI * Math.random();
-        let phi = Math.acos(2 * Math.random() - 1);
-        let dx = 0 + (radius * Math.sin(phi) * Math.cos(theta));
-        let dy = 0 + (radius * Math.sin(phi) * Math.sin(theta));
-        let dz = 0 + (radius * Math.cos(phi));
-        return new THREE.Vector3(dx, dy, dz);
-    }
+    let lines = new THREE.LineSegments(linesGeometry, starsMaterial);
+    linesGeometry.getAttribute("position").setUsage(THREE.DynamicDrawUsage);
+    scene.add(lines);
 }
 
 
+
+
 function animate() {
-
-    //Stars  Animation
-    stars.geometry.vertices.forEach(function (v) {
-        v.x += (0 - v.x) / v.velocity;
-        v.y += (0 - v.y) / v.velocity;
-        v.z += (0 - v.z) / v.velocity;
-
-        v.velocity -= 0.3;
-
-        if (v.x <= 5 && v.x >= -5 && v.z <= 5 && v.z >= -5) {
-            v.x = v.startX;
-            v.y = v.startY;
-            v.z = v.startZ;
-            v.velocity = THREE.MathUtils.randInt(50, 300);
-        }
-    });
-
-
-    //Nucleus Animation
-    nucleus.geometry.vertices.forEach(function (v) {
-        let time = Date.now();
-        v.normalize();
-        let distance = nucleus.geometry.parameters.radius + noise.noise3D(
-            v.x + time * 0.0005,
-            v.y + time * 0.0003,
-            v.z + time * 0.0008
-        ) * blobScale;
-        v.multiplyScalar(distance);
-    })
-    nucleus.geometry.verticesNeedUpdate = true;
-    nucleus.geometry.normalsNeedUpdate = true;
-    nucleus.geometry.computeVertexNormals();
-    nucleus.geometry.computeFaceNormals();
-    nucleus.rotation.y += 0.002;
-
-
-    //Sphere Beckground Animation
+    planet.rotation.y += 0.002;
     sphereBg.rotation.x += 0.002;
     sphereBg.rotation.y += 0.002;
     sphereBg.rotation.z += 0.002;
 
-    
-    controls.update();
-    stars.geometry.verticesNeedUpdate = true;
+
+    // Moon Animation
+    moon.rotation.y -= 0.007;
+    moon.rotation.x -= 0.007;
+    moon.position.x = 15 * Math.cos(t) + 0;
+    moon.position.z = 20 * Math.sin(t) - 35;
+    t += 0.015;
+
+
+    // Terrain Animation  
+    let t_vertex_Array = terrainGeometry.getAttribute("position").array;
+    let t_myZ_Array = terrainGeometry.getAttribute("myZ").array;
+
+    for (let i = 0; i < t_vertex_Array.length; i++) {
+        if (i >= 210 && i <= 250) t_vertex_Array[i * 3 + 2] = 0;
+        else {
+            t_vertex_Array[i * 3 + 2] = Math.sin((i + count * 0.0003)) * (t_myZ_Array[i] - (t_myZ_Array[i] * 0.5));
+            count += 0.1;
+        }
+    }
+
+    //   Stars Animation  
+    for (let i = 0; i < lineTotal; i++) {
+        l_velocity_Array[2 * i] += 0.0049;
+        l_velocity_Array[2 * i + 1] += 0.005;
+
+        l_vertex_Array[6 * i + 2] += l_velocity_Array[2 * i];
+        l_vertex_Array[6 * i + 5] += l_velocity_Array[2 * i + 1];
+
+        if (l_vertex_Array[6 * i + 2] > 50) {
+            l_vertex_Array[6 * i + 2] = l_vertex_Array[6 * i + 5] = THREE.MathUtils.randInt(-200, 10);
+            l_velocity_Array[2 * i] = 0;
+            l_velocity_Array[2 * i + 1] = 0;
+        }
+    }
+
+
+    //Camera Movement
+    camera.position.x += cameraDx;
+    camera.position.y = -1.2 * (1 - Math.abs(frame / 2000 - 0.5) / 0.5);
+    camera.lookAt(0, 0, 0);
+    frame += 8;
+    if (frame > 2000) frame = 0;
+    if (camera.position.x > 18) cameraDx = -cameraDx;
+    if (camera.position.x < -18) cameraDx = Math.abs(cameraDx);
+
+
+    l_positionAttr.needsUpdate = true;
+    terrainGeometry.attributes.position.needsUpdate = true;
     renderer.render(scene, camera);
     requestAnimationFrame(animate);
 }
+
 
 
 
@@ -198,18 +227,18 @@ function onWindowResize() {
 
 
 /*     Fullscreen btn     */
-// let fullscreen;
-// let fsEnter = document.getElementById('fullscr');
-// fsEnter.addEventListener('click', function (e) {
-//     e.preventDefault();
-//     if (!fullscreen) {
-//         fullscreen = true;
-//         document.documentElement.requestFullscreen();
-//         fsEnter.innerHTML = "Exit Fullscreen";
-//     }
-//     else {
-//         fullscreen = false;
-//         document.exitFullscreen();
-//         fsEnter.innerHTML = "Go Fullscreen";
-//     }
-// });
+let fullscreen;
+let fsEnter = document.getElementById('fullscr');
+fsEnter.addEventListener('click', function (e) {
+    e.preventDefault();
+    if (!fullscreen) {
+        fullscreen = true;
+        document.documentElement.requestFullscreen();
+        fsEnter.innerHTML = "Exit Fullscreen";
+    }
+    else {
+        fullscreen = false;
+        document.exitFullscreen();
+        fsEnter.innerHTML = "Go Fullscreen";
+    }
+});
